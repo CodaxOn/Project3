a4ce095d8e712d7bdbd22b04fed1950c3cbde76b
younes93600 <youneslbs93@gmail.com>
2025-11-27 08:17:10 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,5 @@
 <?php
-session_start(); // Toujours mettre ça au début
+session_start(); // Toujours mettre ça au débuth
 
 // Si l'utilisateur n'est pas connecté, on le vire !
 if (!isset($_SESSION['user_id'])) {
